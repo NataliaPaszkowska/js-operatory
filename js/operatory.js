8d7bf4bd86774b013aa0e0f619c48064f49081e9
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-10 14:35:01 +0200

    commit

---
+++ js/operatory.js
@@ -0,0 +1,71 @@
+'use strict'
+
+/*var liczba1 = 13, liczba2 = 5;
+
+var wynik;
+
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+var licz = 2;
+licz += 3;
+ licz = licz + 3 
+
+console.log(licz);*/
+
+
+/*
+var liczba1 = "13", liczba2 = 13;
+
+if (liczba1 == liczba2 ) {
+	console.log("rowne");
+}
+
+var liczba1 = "13", liczba2 = 13;
+
+if (liczba1 === liczba2 ) {
+	console.log("rowne");
+}
+*/
+
+/*
+var liczba1 = 13, liczba2 = 5;
+while (liczba1 == liczba2) {
+	console.log(liczba1);
+	
+}  - to jest jakis przypal
+*/
+
+/*
+var liczba1 = 13, liczba2 = 5;
+if ( (liczba2 > 3) && (liczba2 < 10) ) {
+	console.log("tak");
+}
+*/
+
+
+/*var liczba1 = 13, liczba2 = 5;
+if ( ! (liczba2 > 3) && (liczba2 < 10) ) {
+	console.log("tak");
+} - wykrzynik zaprzecza warunkowi  */
+
+if (2>0) {
+	console.log("tak")
+} else {
+	console.log("nie");
+}
+/*
+(2>5) ? console.log("tak") : console.log("nie"); - skrocona wersja tego co na gorze*/
+
+
+
+
+
+
+
+
+
+
+
+
+
